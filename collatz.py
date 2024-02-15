63cd9e7e2240461a8bc2dbe5c99be6a5550e6f74
Bharath Reddy <bnalla68@gmail.com>
2024-02-15 02:51:57 -0500

    plot_collatz function

--- collatz.py
+++ collatz.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def collatz(n):
     if n % 2 == 0:
@@ -6,20 +7,20 @@ def collatz(n):
     else:
         return int((3*n + 1)/2)
 
-xlist = []
-ylist = []
+def plot_collatz(b):
+    ylist = []
+    while b != 1:
+        ylist.append(b)
+        b = collatz(b)
+    xlist = range(1, len(ylist) + 1)  # Generate xlist based on the length of ylist
+    plt.plot(xlist, ylist)
 
 num = input("Enter a number:")
-num = int(num)
-
 b = int(num)
 
-while b != 1:
-    a = b
-    b = collatz(b)
-    xlist.append(a)
-    ylist.append(b)
+# for b in range(1,27):
+#     plot_collatz(b)
 
-plt.figure(num=0, dpi = 150)
-plt.plot(xlist,ylist)
+plot_collatz(b)
+plt.tight_layout()
 plt.show()
